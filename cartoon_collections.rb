46e513760bc857bc3ead8ae826b43f5404e2e4f7
HELEN HARMAN <hlharman@bellsouth.net>
2020-11-30 01:51:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,8 @@ def find_the_cheese(ingredients)
     ingredients.each do |element|
   if cheese_types.include? (element)
      return element 
-     else   
+   else
+     cheese_types.!include? (element)
      return "nil"
      end 
  end 
