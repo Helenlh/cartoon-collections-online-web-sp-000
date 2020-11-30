c1231f882f1f5e3315649ac2419713cf2f79b49b
HELEN HARMAN <hlharman@bellsouth.net>
2020-11-30 01:01:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ def long_planeteer_calls(words)
 
 def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.each do |cheese|
+  ingredients.each do |cheese|
   if cheese.include?(ingredients)
     puts cheese 
   end 
